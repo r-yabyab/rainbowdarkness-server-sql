14e71e9475ecda32c375f8f0aa96940459ea7c8f
Scruffy pc <cayabyabrr@yahoo.com>
2023-10-01 20:43:26 -0700

    sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE rainbowdarkness;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE rainbows(
     rainbow_id SERIAL PRIMARY KEY,
-    mood FLOAT(2, 2),
+    mood FLOAT(2, 1),
     createdAt TIMESTAMP with TIME ZONE,
     updatedAt TIMESTAMP with TIME ZONE,
     userID VARCHAR(255)
